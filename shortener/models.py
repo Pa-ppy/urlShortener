fb26002deb0e65a0c6affd44f86ed8dbb00a36dd
Pa-ppy <rayjacks07@gmail.com>
2025-07-09 15:58:30 +0000

    Database models definition for shortener app

--- shortener/models.py
+++ shortener/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 
 class URL(models.Model):
-    original_url = models.URLField(unique=True)
+    original_url = models.URLField(unique=True, max_length=2048)
     short_code = models.CharField(max_length=15, unique=True, blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
     clicks = models.PositiveIntegerField(default=0)
